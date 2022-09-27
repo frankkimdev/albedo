d87caea1be1db494d842d262725f230fc1cfb8b3
Kim kihyun <khkim3311@gsretail.com>
2022-09-27 13:45:39 +0900

    Initial commit (page 40)

---
+++ src/main/java/com/astronaut/albedo/AlbedoApplication.java
@@ -0,0 +1,13 @@
+package com.astronaut.albedo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlbedoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlbedoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/astronaut/albedo/AlbedoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.astronaut.albedo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlbedoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
