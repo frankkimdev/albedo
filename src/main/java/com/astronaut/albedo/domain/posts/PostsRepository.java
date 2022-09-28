d1756b51b4e8d6fb78fee079daf2ff22c1e2b65f
Kim kihyun <khkim3311@gsretail.com>
2022-09-28 14:17:43 +0900

    # JPA 시작

---
+++ src/main/java/com/astronaut/albedo/domain/posts/PostsRepository.java
@@ -0,0 +1,8 @@
+package com.astronaut.albedo.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+
+}
---
+++ src/main/java/com/astronaut/albedo/web/PostsApiController.java
@@ -0,0 +1,4 @@
+package com.astronaut.albedo.web;
+
+public class PostsApiController {
+}
