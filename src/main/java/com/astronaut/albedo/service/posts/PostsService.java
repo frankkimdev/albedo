ad6f06386e2272ec759d9389b1252b0456b35f3c
greenerous <rlgus33112365@gmail.com>
2022-10-09 01:58:38 +0900

    ~ 150 page 151부터

--- src/main/java/com/astronaut/albedo/service/posts/PostsService.java
+++ src/main/java/com/astronaut/albedo/service/posts/PostsService.java
@@ -2,6 +2,7 @@ package com.astronaut.albedo.service.posts;
 
 import com.astronaut.albedo.domain.posts.Posts;
 import com.astronaut.albedo.domain.posts.PostsRepository;
+import com.astronaut.albedo.web.dto.PostsListResponseDto;
 import com.astronaut.albedo.web.dto.PostsResponseDto;
 import com.astronaut.albedo.web.dto.PostsSaveRequestDto;
 import com.astronaut.albedo.web.dto.PostsUpdateRequestDto;
@@ -9,13 +10,21 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RequiredArgsConstructor
 @Service
 public class PostsService {
     private final PostsRepository postsRepository;
 
     //frank_북마크 : 148 page
-
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
 
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
